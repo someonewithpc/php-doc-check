c7d5b54221b48d2ecffcdbdda70d61f3aebe9827
Hugo Sales <hugo@hsal.es>
2021-12-30 14:51:41 +0000

    Update composer to allow for newer packages, avoiding deprecations

--- src/ApplicationArgumentsProvider.php
+++ src/ApplicationArgumentsProvider.php
@@ -2,60 +2,65 @@
 
 namespace NdB\PhpDocCheck;
 
+use GetOpt\ArgumentException;
+use GetOpt\GetOpt;
+use GetOpt\Operand;
+use GetOpt\Option;
+
 class ApplicationArgumentsProvider
 {
     protected $getOpt;
     public function __construct()
     {
-        $this->getOpt = new \GetOpt\GetOpt([
-            \GetOpt\Option::create('x', 'exclude', \GetOpt\GetOpt::MULTIPLE_ARGUMENT)
+        $this->getOpt = new GetOpt([
+            Option::create('x', 'exclude', GetOpt::MULTIPLE_ARGUMENT)
                 ->setDescription('Directories to exclude, without slash'),
-            \GetOpt\Option::create('f', 'format', \GetOpt\GetOpt::REQUIRED_ARGUMENT)
+            Option::create('f', 'format', GetOpt::REQUIRED_ARGUMENT)
                 ->setDescription('Output format [text, json] [default: text]')
                 ->setDefaultValue('text'),
-            \GetOpt\Option::create('o', 'reportFile', \GetOpt\GetOpt::REQUIRED_ARGUMENT)
+            Option::create('o', 'reportFile', GetOpt::REQUIRED_ARGUMENT)
                 ->setDescription('Send report output to a file')
                 ->setDefaultValue(''),
-            \GetOpt\Option::create('m', 'metric', \GetOpt\GetOpt::MULTIPLE_ARGUMENT)
+            Option::create('m', 'metric', GetOpt::MULTIPLE_ARGUMENT)
                 ->setDescription(
                     'Metric to use for determining complexity [metrics.complexity.cognitive, '.
                     'metrics.complexity.cyclomatic, metrics.deprecated.category, metrics.deprecated.subpackage, '.
                     'metrics.complexity.length] [default: metrics.complexity.cognitive]'
                 )
                 ->setDefaultValue('metrics.complexity.cognitive'),
-            \GetOpt\Option::create('w', 'complexity-warning-threshold', \GetOpt\GetOpt::REQUIRED_ARGUMENT)
+            Option::create('w', 'complexity-warning-threshold', GetOpt::REQUIRED_ARGUMENT)
                 ->setDescription('Cyclomatic complexity score which is the lower bound for a warning [default: 4]')
                 ->setDefaultValue(4)->setValidation('is_numeric'),
-            \GetOpt\Option::create('e', 'complexity-error-threshold', \GetOpt\GetOpt::REQUIRED_ARGUMENT)
+            Option::create('e', 'complexity-error-threshold', GetOpt::REQUIRED_ARGUMENT)
                 ->setDescription('Cyclomatic complexity score which is the lower bound for an error [default: 6]')
                 ->setDefaultValue(6)->setValidation('is_numeric'),
-            \GetOpt\Option::create('$', 'file-extension', \GetOpt\GetOpt::MULTIPLE_ARGUMENT)
+            Option::create('$', 'file-extension', GetOpt::MULTIPLE_ARGUMENT)
                 ->setDescription('Valid file extensions to scan [default: php]')
                 ->setDefaultValue('php'),
-            \GetOpt\Option::create('g', 'grouping-method', \GetOpt\GetOpt::REQUIRED_ARGUMENT)
+            Option::create('g', 'grouping-method', GetOpt::REQUIRED_ARGUMENT)
                 ->setDescription(
                     'Allows different grouping of the results list '.
                     '[file, none, metric, severity, fileline] [default: file]'
                 )
                 ->setDefaultValue('file'),
-            \GetOpt\Option::create('s', 'sorting-method', \GetOpt\GetOpt::REQUIRED_ARGUMENT)
+            Option::create('s', 'sorting-method', GetOpt::REQUIRED_ARGUMENT)
                 ->setDescription(
                     'Sorting for the results. Natural sorts by name for groups and line for findings. '.
                     'Value uses the cumulative group score, and finding score as sorting value. '.
                     '[natural, value] [default: natural]'
                 )
                 ->setDefaultValue('file'),
-            \GetOpt\Option::create('i', 'ignore-violations-on-exit', \GetOpt\GetOpt::NO_ARGUMENT)
+            Option::create('i', 'ignore-violations-on-exit', GetOpt::NO_ARGUMENT)
                 ->setDescription('Will exit with a zero code, even if any violations are found'),
-            \GetOpt\Option::create('?', 'help', \GetOpt\GetOpt::NO_ARGUMENT)
+            Option::create('?', 'help', GetOpt::NO_ARGUMENT)
                 ->setDescription('Show this help and quit'),
-            \GetOpt\Option::create('q', 'quiet', \GetOpt\GetOpt::NO_ARGUMENT)
+            Option::create('q', 'quiet', GetOpt::NO_ARGUMENT)
                 ->setDescription('Don\'t show any output'),
         ]);
         $this->getOpt->addOperand(
-            new \GetOpt\Operand(
+            new Operand(
                 'directory',
-                \GetOpt\Operand::MULTIPLE+\GetOpt\Operand::REQUIRED
+                Operand::MULTIPLE+Operand::REQUIRED
             )
         );
     }
@@ -63,11 +68,11 @@ class ApplicationArgumentsProvider
     /**
      * @SuppressWarnings(PHPMD.ExitExpression)
      */
-    public function getArguments($arguments = null):\GetOpt\GetOpt
+    public function getArguments($arguments = null):GetOpt
     {
         try {
             $this->getOpt->process($arguments);
-        } catch (\GetOpt\ArgumentException $e) {
+        } catch (ArgumentException $e) {
             echo $e->getMessage() . "\n";
             echo $this->getOpt->getHelpText();
             exit;
